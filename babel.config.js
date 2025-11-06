f29f4f8d6900958f8d0c83b8c24a4de944569c4e
ThabeloPeter <thabelopetertshivhidzo@gmail.com>
2025-11-06 10:46:08 +0200

    Fix web build: Conditionally disable reanimated plugin for web to avoid worklets dependency

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,25 @@
 module.exports = function(api) {
   api.cache(true);
+  
+  const presets = [
+    ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+    "nativewind/babel",
+  ];
+  
+  const plugins = [];
+  
+  // Only add reanimated plugin for native platforms (not web)
+  // Skip for web builds to avoid worklets dependency
+  // When building for web, EXPO_PUBLIC_PLATFORM will be 'web'
+  const isWeb = process.env.EXPO_PUBLIC_PLATFORM === 'web';
+  
+  if (!isWeb) {
+    plugins.push("react-native-reanimated/plugin"); // Must be last
+  }
+  
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      "react-native-reanimated/plugin", // Must be last
-    ],
+    presets,
+    plugins,
   };
 };
 
