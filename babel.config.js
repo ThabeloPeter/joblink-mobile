8a38b4c40d3f9e686723272402c370456f20336c
ThabeloPeter <thabelopetertshivhidzo@gmail.com>
2025-11-06 10:48:50 +0200

    Install react-native-worklets-core and configure babel-preset-expo to disable reanimated for web

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,21 @@
 module.exports = function(api) {
   api.cache(true);
   
+  const isWeb = process.env.EXPO_PUBLIC_PLATFORM === 'web';
+  
   const presets = [
-    ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+    [
+      "babel-preset-expo",
+      {
+        jsxImportSource: "nativewind",
+        // Disable reanimated for web builds
+        web: {
+          transform: {
+            reactNativeReanimated: false,
+          },
+        },
+      },
+    ],
     "nativewind/babel",
   ];
   
@@ -10,9 +23,6 @@ module.exports = function(api) {
   
   // Only add reanimated plugin for native platforms (not web)
   // Skip for web builds to avoid worklets dependency
-  // When building for web, EXPO_PUBLIC_PLATFORM will be 'web'
-  const isWeb = process.env.EXPO_PUBLIC_PLATFORM === 'web';
-  
   if (!isWeb) {
     plugins.push("react-native-reanimated/plugin"); // Must be last
   }
